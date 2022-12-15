c9d2a6092796dbb20212922b7a83c1af24269267
Medhat Riad <medhat.riad@gmail.com>
2022-12-15 13:28:20 +0100

    Modify default app to make it more personal

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
 import logo from './logo.svg';
 import './App.css';
+import YouTubeSubscribe from './components/YoutubeSubscribe';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header>Header</header>
+      <div className='content'>
+        <pre className='crt'>
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            This is a React application!
+          </p>
+        </pre>
+        <div className='subscribe'>
+            <YouTubeSubscribe/>  
+        </div>
+      </div>
+      
+      <footer>
+      <div className="right">
+            <a href="https://github.com/hackenshaw">
+                <img className="github-logo" alt="" src="images/github-logo.png"/>
+            </a>
+        
+            <a href="https://www.youtube.com/channel/UCZEnbGBfukY7hukNcRr1vjA">
+                <img className="yt-logo" alt="" src="images/yt-logo.png"/>
+            </a>
+        
+            <a href="https://www.instagram.com/hackenshaw/">
+                <img className="insta-logo" alt="" src="images/instagram-logo.png"/>
+            </a>
+        </div>
+      </footer>
+      
     </div>
   );
 }
